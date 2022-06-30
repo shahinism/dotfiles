1d619120739b5b84f2ce4602bf5bb875debb4dfb
Shahin Azad <hi@shahinism.com>
2022-06-30 11:57:39 +0200

    add typescript basic config.
    Signed-off-by: Reza (Shahin) Khanipour <hi@shahinism.com>

---
+++ .emacs.d/modules/lt-typescript.el
@@ -0,0 +1,24 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+(lt/install-package 'typescript-mode)
+(lt/install-package 'js2-refactor)
+(lt/install-package 'tide)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(provide 'lt-typescript)
