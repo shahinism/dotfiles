72ae86bc671107f5a693082847fb947ab1db94ca
Shahin Azad <hi@shahinism.com>
2022-07-03 12:43:48 +0200

    switch back to Emacs electric for pairing, disable it on org <.
    Signed-off-by: Reza (Shahin) Khanipour <hi@shahinism.com>

--- .emacs.d/modules/lt-editing.el
+++ .emacs.d/modules/lt-editing.el
@@ -1,11 +1,9 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-(lt/install-package 'smartparens)
-(require 'smartparens-config)
-;; (sp-local-pair 'org-mode "*" nil :actions :rem)
-(smartparens-global-mode)
+;; parentheses
+(electric-pair-mode 1)
 
 (show-paren-mode 1)
-
 (setq show-paren-style 'mixed)
+
 (provide 'lt-editing)
