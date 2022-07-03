ecf549a3c4ad5fd39c7654379572fed5448581f9
Shahin Azad <hi@shahinism.com>
2022-07-03 12:56:10 +0200

    drop window hydra.
    Signed-off-by: Reza (Shahin) Khanipour <hi@shahinism.com>

--- .emacs.d/modules/lt-navigation.el
+++ .emacs.d/modules/lt-navigation.el
@@ -28,49 +28,5 @@
 (global-set-key (kbd "C-j") #'hydra-avy/body)
 (global-set-key (kbd "C-<return>") #'newline-and-indent)
 
-(defhydra hydra-window (:color red
-                        :hint nil)
-  "
- Split: _-_ert _/_:horz
-Delete: _o_nly  _da_ce  _dw_indow  _db_uffer  _df_rame
-  Move: _s_wap
-Frames: _f_rame new  _df_ delete
-  Misc: _m_ark _a_ce  _u_ndo  _r_edo"
-  ("h" windmove-left)
-  ("j" windmove-down)
-  ("k" windmove-up)
-  ("l" windmove-right)
-  ("H" hydra-move-splitter-left)
-  ("J" hydra-move-splitter-down)
-  ("K" hydra-move-splitter-up)
-  ("L" hydra-move-splitter-right)
-  ("|" (lambda ()
-         (interactive)
-         (split-window-right)
-         (windmove-right)))
-  ("_" (lambda ()
-         (interactive)
-         (split-window-below)
-         (windmove-down)))
-  ("/" split-window-right)
-  ("-" split-window-below)
-  ;("t" transpose-frame "'")
-  ;; winner-mode must be enabled
-  ("u" winner-undo)
-  ("r" winner-redo) ;;Fixme, not working?
-  ("o" delete-other-windows :exit t)
-  ("a" ace-window :exit t)
-  ("f" new-frame :exit t)
-  ("s" ace-swap-window)
-  ("da" ace-delete-window)
-  ("dw" delete-window)
-  ("db" kill-this-buffer)
-  ("df" delete-frame :exit t)
-  ("q" nil)
-  ;("i" ace-maximize-window "ace-one" :color blue)
-  ;("b" ido-switch-buffer "buf")
-  ("m" headlong-bookmark-jump))
-
-(global-set-key (kbd "C-w") #'hydra-window/body)
 
 (provide 'lt-navigation)
