3f38cff45c99930b277af71454f47d1b89b3d4bd
Reza (Shahin) Khanipour <hi@shahinism.com>
2022-07-10 11:25:32 +0200

    add editorconfig support.

--- .emacs.d/modules/lt-editing.el
+++ .emacs.d/modules/lt-editing.el
@@ -1,9 +1,15 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+(lt/install-package 'editorconfig)
+
 ;; parentheses
 (electric-pair-mode 1)
 
 (show-paren-mode 1)
 (setq show-paren-style 'mixed)
+
+;; Use settings from .editorconfig file when present
+(require 'editorconfig)
+(editorconfig-mode 1)
 
 (provide 'lt-editing)
