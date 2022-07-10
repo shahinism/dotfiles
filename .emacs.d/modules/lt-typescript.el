69e840a92860d223b25138be42844a6b23d7fc66
Reza (Shahin) Khanipour <hi@shahinism.com>
2022-07-10 12:20:45 +0200

    switch to eglot and typescript-language-server instead of tide.

--- .emacs.d/modules/lt-typescript.el
+++ .emacs.d/modules/lt-typescript.el
@@ -2,19 +2,9 @@
 
 (lt/install-package 'typescript-mode)
 (lt/install-package 'js2-refactor)
-(lt/install-package 'tide)
 
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
+(when (fboundp 'eglot-ensure)
+  (progn
+    (add-hook 'typescript-mode-hook #'eglot-ensure)))
 
 (provide 'lt-typescript)
