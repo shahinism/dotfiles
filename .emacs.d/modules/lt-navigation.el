5f36989c834d6fddd087d29f3e494417883206ee
Reza (Shahin) Khanipour <hi@shahinism.com>
2022-07-25 11:20:47 +0200

    add expand region.

--- .emacs.d/modules/lt-navigation.el
+++ .emacs.d/modules/lt-navigation.el
@@ -1,7 +1,11 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
 (lt/install-package 'evil-easymotion)
+(lt/install-package 'expand-region)
+
+;; Configuration
 (evilem-default-keybindings ",")
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 
 (provide 'lt-navigation)
