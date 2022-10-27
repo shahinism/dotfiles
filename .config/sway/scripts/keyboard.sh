b0f5b9afe34a827425defe86750405b0a551ab4e
Reza (Shahin) Khanipour <hi@shahinism.com>
2022-10-27 13:54:33 +0200

    fix layout change issue

--- .config/sway/scripts/keyboard.sh
+++ .config/sway/scripts/keyboard.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-# script that sets the locale from current locale settings
-swaymsg input type:keyboard xkb_layout "$(localectl status | grep "X11 Layout" | sed -e "s/^.*X11 Layout://")"
 
-if localectl status | grep "X11 Variant" ; then
-    swaymsg input type:keyboard xkb_variant "$(localectl status | grep "X11 Variant" | sed -e "s/^.*X11 Variant://")"
-fi
+# NOTE this was preventing sway input to change the layout. When this
+# runs on autostart, no other layout than the main layout is available
+# for xkb.
+
+# script that sets the locale from current locale settings
+# swaymsg input type:keyboard xkb_layout "$(localectl status | grep "X11 Layout" | sed -e "s/^.*X11 Layout://")"
+
+# if localectl status | grep "X11 Variant" ; then
+#     swaymsg input type:keyboard xkb_variant "$(localectl status | grep "X11 Variant" | sed -e "s/^.*X11 Variant://")"
+# fi
