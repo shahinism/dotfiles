e51cdecfc642f3a5b5a186f6af655cd6a6faf69e
Reza (Shahin) Khanipour <hi@shahinism.com>
2022-11-10 18:35:49 +0100

    enable flyspell.

--- .emacs.d/modules/lt-editing.el
+++ .emacs.d/modules/lt-editing.el
@@ -1,7 +1,8 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
 ;; DirVish
-(use-package dirvish
+(leaf dirvish
+  :ensure t
   :config
   (dirvish-override-dired-mode)
   (setq dirvish-attributes '(all-the-icons file-size collapse subtree-state vc-state git-msg))
@@ -14,9 +15,33 @@
 (setq show-paren-style 'mixed)
 
 ;; Use settings from .editorconfig file when present
-(use-package editorconfig
-  :config
-  (editorconfig-mode 1)
-  )
+(leaf editorconfig
+  :ensure t
+  :global-minor-mode editorconfig-mode)
+
+
+;; flyspell + UI
+(leaf flyspell
+  :doc "Spell checker"
+  :url "https://www.emacswiki.org/emacs/FlySpell"
+  :ensure t
+  :hook
+  (prog-mode-hook . flyspell-prog-mode)
+  ((markdown-mode-hook git-commit-mode-hook) . flyspell-mode)
+  :custom
+  (ispell-program-name . "aspell")
+  (ispell-extra-args   . '("--sug-mode=ultra" "--lang=en_US" "--run-together"))
+  :custom-face
+  (flyspell-incorrect  . '((t (:underline (:color "#f1fa8c" :style wave)))))
+  (flyspell-duplicate  . '((t (:underline (:color "#50fa7b" :style wave))))))
+
+(leaf flyspell-correct
+  :doc "Correcting misspelled words with flyspell using favourite interface"
+  :url "https://github.com/d12frosted/flyspell-correct"
+  :ensure t
+  :bind*
+  ("C-M-i" . flyspell-correct-at-point)
+  :custom
+  (flyspell-correct-interface . #'flyspell-correct-completing-read))
 
 (provide 'lt-editing)
