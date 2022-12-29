b0dc4915aef301d3023b08cb7d5f0a14af3aa2df
Reza (Shahin) Khanipour <hi@shahinism.com>
2022-12-29 13:51:29 +0100

    apply capture templates for org roam.

--- .emacs.d/modules/lt-note.el
+++ .emacs.d/modules/lt-note.el
@@ -59,8 +59,45 @@
   :init
   (setq org-roam-v2-ack t
         org-directory (concat (getenv "HOME") "/Documents/notes/")
-        org-roam-directory (file-truename org-directory))
+        org-roam-directory (file-truename org-directory)
+        org-roam-capture-templates
+        '(("m" "main" plain
+           "%?"
+           :if-new (file+head "main/${slug}.org"
+                              "#+title: ${title}\n")
+           :immediate-finish t
+           :unnarrowed t)
+          ("r" "reference" plain "%?"
+           :if-new
+           (file+head "reference/${title}.org" "#+title: ${title}\n")
+           :immediate-finish t
+           :unnarrowed t)
+          ("a" "article" plain "%?"
+           :if-new
+           (file+head "articles/${title}.org" "#+title: ${title}\n#+filetags: :article:\n")
+           :immediate-finish t
+           :unnarrowed t)))
 
+  (cl-defmethod org-roam-node-type ((node org-roam-node))
+  "Return the TYPE of NODE."
+  (condition-case nil
+      (file-name-nondirectory
+       (directory-file-name
+        (file-name-directory
+         (file-relative-name (org-roam-node-file node) org-roam-directory))))
+    (error "")))
+
+  
+  ;; Every zettle is a draft unless defined otherwise
+  ;; https://jethrokuan.github.io/org-roam-guide/
+  (defun jethro/tag-new-node-as-draft ()
+    (org-roam-tag-add '("draft")))
+  (add-hook 'org-roam-capture-new-node-hook #'jethro/tag-new-node-as-draft)
+
+  (setq org-roam-node-display-template
+      (concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+
+  
   (unless (f-directory? org-roam-directory)
     (make-directory org-roam-directory t))
 
