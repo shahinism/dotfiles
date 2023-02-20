2381ce9e86f96eaf4b4bff9d370335ccadd00727
Reza (Shahin) Khanipour <hi@shahinism.com>
2023-02-20 18:48:55 +0100

    nix: Enable zsh in impure shells.

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -38,6 +38,7 @@
     firefox
     brave
     slack
+    xclip
     git     # required for Emacs
     direnv  # required for `zplug: plugins/direnv`
     fzf
@@ -51,8 +52,26 @@
     pcscliteWithPolkit
   ];
 
+  home.shellAliases = {
+    c = "xclip -selection clipboard";
+  };
+
   programs.zsh = {
     enable = true;
+
+    # https://github.com/chisui/zsh-nix-shell
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
 
     zplug = {
       enable = true;
@@ -67,6 +86,7 @@
         {name = "zdharma-continuum/fast-syntax-highlighting";}
       ];
     };
+
     initExtra = "source ${./zsh/initExtra.zsh}";
   };
 
