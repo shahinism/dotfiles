b1f7c06d5d82adf8f11197d58cc17bacc68d9140
Reza (Shahin) Khanipour <hi@shahinism.com>
2023-02-24 17:16:13 +0100

    feat (firefox): Install customized/fast/private version of Firefox.

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,4 +2,10 @@
   # Allow to install proprietary or unfree packages
   # https://bit.ly/3Zmmfm3
   allowUnfree = true;
+
+  packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
 }
